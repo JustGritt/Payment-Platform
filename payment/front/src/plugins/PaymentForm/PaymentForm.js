07f8bb2e3eb0766ade2d212c81050c14131d544f
alex_root <alexdieudonne02@gmail.com>
2023-07-24 15:06:17 +0200

    puchass 2

--- payment/front/src/plugins/PaymentForm/PaymentForm.js
+++ payment/front/src/plugins/PaymentForm/PaymentForm.js
@@ -1,6 +1,10 @@
+import Form from "./components/Form.vue";
 import Home from "./components/Home.vue";
 import config from './formkit.config.js'
+import 'vue-select/dist/vue-select.css';
 import { plugin, defaultConfig } from "@formkit/vue";
+import VueCreditCardValidation from 'vue-credit-card-validation';
+import "@vemlavaraloucagamers/vg-credit-card/dist/style.css";
 
 // import "@formkit/themes/genesis";
 
@@ -9,5 +13,6 @@ export default {
     install: (app, options) => {
         app.use(plugin, defaultConfig(config))
         app.component("payment-form", Home);
+        app.use(VueCreditCardValidation);
     },
 };
