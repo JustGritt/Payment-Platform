e6681d9be87fbbeb2510fc6cfa440653804030ea
alex_root <alexdieudonne02@gmail.com>
2023-07-24 15:08:00 +0200

    fix not working card

--- payment/front/src/plugins/PaymentForm/PaymentForm.js
+++ payment/front/src/plugins/PaymentForm/PaymentForm.js
@@ -1,10 +1,7 @@
-import Form from "./components/Form.vue";
 import Home from "./components/Home.vue";
 import config from './formkit.config.js'
-import 'vue-select/dist/vue-select.css';
 import { plugin, defaultConfig } from "@formkit/vue";
 import VueCreditCardValidation from 'vue-credit-card-validation';
-import "@vemlavaraloucagamers/vg-credit-card/dist/style.css";
 
 // import "@formkit/themes/genesis";
 
