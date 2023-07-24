5c2a4332610089f0c8478a1139d85392dea44597
alex_root <alexdieudonne02@gmail.com>
2023-07-24 14:51:55 +0200

    v-select removed

--- payment/front/src/plugins/PaymentForm/PaymentForm.js
+++ payment/front/src/plugins/PaymentForm/PaymentForm.js
@@ -1,16 +1,13 @@
 import Home from "./components/Home.vue";
-import vSelect from 'vue-select'
 import config from './formkit.config.js'
 import 'vue-select/dist/vue-select.css';
 import { plugin, defaultConfig } from "@formkit/vue";
-import "@vemlavaraloucagamers/vg-credit-card/dist/style.css";
 
 // import "@formkit/themes/genesis";
 
 
 export default {
     install: (app, options) => {
-        app.component('v-select', vSelect)
         app.use(plugin, defaultConfig(config))
         app.component("payment-form", Home);
     },
