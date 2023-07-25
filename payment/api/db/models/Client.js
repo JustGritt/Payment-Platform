1c7ddecb2195555da91d72f1e778eeedb000a957
Alexis TAN <alexistan9712@gmail.com>
2023-07-25 14:32:33 +0200

    ✨ Hotfix models & Add tests

--- payment/api/db/models/Client.js
+++ payment/api/db/models/Client.js
@@ -1,40 +1,70 @@
-const { DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
 module.exports = function (connection) {
-    const Client = connection.define("Client", {
+    class Client extends Model {
+        async checkPassword(password) {
+            const bcrypt = require("bcryptjs");
+            return bcrypt.compare(password, this.password);
+        }
+
+        generateToken() {
+            const jwt = require("jsonwebtoken");
+            return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+                expiresIn: "1y",
+            });
+        }
+    }
+
+    Client.init({
+        client_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         firstname: DataTypes.STRING,
         lastname: DataTypes.STRING,
+        phone_number: DataTypes.STRING,
+        address: DataTypes.STRING,
         email: {
             type: DataTypes.STRING,
             unique: true,
             allowNull: false,
             validate: {
                 isEmail: true,
-                notEmpty: {
-                    msg: "Email cannot be empty",
+                notNull: {
+                    msg: "Email cannot be null",
                 },
             },
         },
-        country: DataTypes.STRING,
-        address: DataTypes.STRING,
-        creditCard: {
+        password: {
             type: DataTypes.STRING,
-            unique: true,
             allowNull: false,
             validate: {
-                // isCreditCard: true,
-                notEmpty: {
-                    msg: "You have to provide a valid credit card number",
+                len: {
+                    args: [8],
+                    msg: "Password must be at least 8 characters long",
+                },
+                is: {
+                    args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
+                    msg: "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character",
                 },
             },
-        }
-    }, {
+        },
+    },
+    {
         sequelize: connection,
         tableName: "clients",
+        hooks: {
+            beforeSave: async (client, options) => {
+                if (client.changed("password")) {
+                    const bcrypt = require("bcryptjs");
+                    const salt = await bcrypt.genSalt(10);
+                    const hash = await bcrypt.hash(client.password, salt);
+                    client.password = hash;
+                }
+            },
+        },
     });
-
-    // Uncomment the hook if you have defined the checkEmailValidity function
-    // Client.addHook("beforeCreate", checkEmailValidity);
 
     return Client;
 };
