8cabf608accdf8e0074322d0af41a6f4de475754
Charles <charles258@hotmail.fr>
2023-07-26 18:02:54 +0200

    hotfix login merchant

--- merchant/front/src/router/index.js
+++ merchant/front/src/router/index.js
@@ -1,33 +1,70 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/TheHome.vue'
-import Dashboard from '../views/Dashboard.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/TheHome.vue";
+import Dashboard from "../views/Dashboard.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
-    {
-        path: '/', 
-        name: 'Home',
-        component: Home,
+  {
+    path: "/",
+    name: "Home",
+    meta: {
+        requiresAuth: true,
     },
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        component: Dashboard,
-        meta: {
-            requiresAuth: true,
-        },
-        children: [
-            {
-                path: '/dashboard/settings',
-                name: 'Settings',
-                component: () => import('../views/Settings.vue'),
-            },
-        ],
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginView,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true,
     },
-]
+    children: [
+      {
+        path: "/dashboard/settings",
+        name: "Settings",
+        component: () => import("../views/Settings.vue"),
+      },
+			{
+				path: "/dashboard/orders",
+				name: "Orders",
+				component: () => import("../views/OrderView.vue"),
+			},
+    ],
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+// Navigation guard to check if the user is authenticated
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+    // Check if the route requires authentication
+    if (requiresAuth) {
+      // Check if clientId and clientToken exist in localStorage
+      const clientId = localStorage.getItem("clientId");
+      const clientToken = localStorage.getItem("clientToken");
+
+      if (!clientId || !clientToken) {
+        // If either clientId or clientToken is missing, redirect to the login page
+        next({ name: "Login" });
+      } else {
+        // If both clientId and clientToken exist, proceed with the navigation
+        next();
+      }
+    } else {
+      // If the route doesn't require authentication, proceed with the navigation
+      next();
+    }
+  });
+
+export default router;
