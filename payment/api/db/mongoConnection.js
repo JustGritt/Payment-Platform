db869ca331f07afc4b317d5dd0ea5d10fd934f1d
Charles Parames <55605195+popokola@users.noreply.github.com>
2023-07-28 16:05:27 +0200

    Update mongoConnection.js

--- payment/api/db/mongoConnection.js
+++ payment/api/db/mongoConnection.js
@@ -7,31 +7,31 @@ if (process.env.NODE_ENV === "test") {
 }else {
     const url = process.env.MONGO_URL;
     client = new MongoClient(url);
-}
-
-
-let mongoDb;
-
-async function connect() {
-    await client.connect();
-    mongoDb = client.db('Global'); // database name
-}
-async function createMongoIndexes() {
-    const dbInstance = getDb();
-    const collection = dbInstance.collection('transactions');
     
-    try {
-        await collection.createIndex({
-            transaction_uid: "text",
-            "transaction_history.transaction_state": "text",
-        });
-        console.log("MongoDB indexes created successfully");
-    } catch (err) {
-        console.error("Error creating MongoDB indexes:", err);
+    let mongoDb;
+
+    async function connect() {
+        await client.connect();
+        mongoDb = client.db('Global'); // database name
     }
-}
-function getDb() {
-    return mongoDb;
+
+    async function createMongoIndexes() {
+        const dbInstance = getDb();
+        const collection = dbInstance.collection('transactions');
+    
+        try {
+            await collection.createIndex({
+                transaction_uid: "text",
+                "transaction_history.transaction_state": "text",
+            });
+            console.log("MongoDB indexes created successfully");
+        } catch (err) {
+            console.error("Error creating MongoDB indexes:", err);
+        }
+    }
+    function getDb() {
+        return mongoDb;
+    }
 }
 
 module.exports = { connect, getDb };
